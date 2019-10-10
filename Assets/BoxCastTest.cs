3cbb0f12ce6e9ae9966ab335caf49c120a865936
Satoru Inukai <sairu_0312_kgxb8745@yahoo.co.jp>
2019-10-10 11:25:37 +0900

    プロジェクト作成

---
+++ Assets/BoxCastTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCastTest : MonoBehaviour
+{
+    RaycastHit hit;
+
+    [SerializeField]
+    bool isEnable = false;
+
+    private void OnDrawGizmos()
+    {
+        if (isEnable == false)
+            return;
+
+        var scale = transform.lossyScale.x * 0.5f;
+
+        var isHit = Physics.BoxCast(transform.position,
+                                    Vector3.one * scale,
+                                    -transform.up,
+                                    out hit,
+                                    transform.rotation);
+
+        if(isHit){
+            Gizmos.DrawRay(transform.position, -transform.up * hit.distance);
+            Gizmos.DrawWireCube(transform.position - transform.up * hit.distance,
+                                Vector3.one * scale * 2);
+        }else{
+            Gizmos.DrawRay(transform.position, -transform.up * 100);
+        }
+
+        Debug.Log(hit.distance);
+    }
+}
